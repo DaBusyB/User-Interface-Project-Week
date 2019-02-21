e7c16c50393eacd495ae280f2428c41d8f992a19
Brandi Apetsi <Brandi@Brandis-iMac.local>
2019-02-21 02:05:27 -0500

    cleanup menu.js file

--- js/menu.js
+++ js/menu.js
@@ -2,15 +2,12 @@ const menu = document.querySelector(".menu");
 const menuButton = document.querySelector(".menu-button");
 const buttonImg = document.querySelector('img[alt="menu-button"]');
 
-
 const toggleMenu = () => {
-    menu.classList.toggle("menu--open")
-    
+    menu.classList.toggle("menu--open");
     
     menu.classList.value === "menu menu--open" ? 
     buttonImg.src = 'img/services/nav-hamburger-close.png' :
-    buttonImg.src = 'img/nav-hamburger.png'
+    buttonImg.src = 'img/nav-hamburger.png';
 }
-
 
 menuButton.addEventListener("click", toggleMenu);
