9e6c9914a53af759abc805b1d8bb166912e0b7c3
Brandi Apetsi <Brandi@Brandis-iMac.local>
2019-02-23 03:49:00 -0500

    working on js tab components

--- js/services.js
+++ js/services.js
@@ -3,31 +3,51 @@ class ButtonLink {
         this.buttonElement = buttonElement;
 
         this.buttonData = this.buttonElement.dataset.button;
-        console.log(this.buttonData)
-        
+       
+        if(this.buttonData === "defaultButton") {
+            this.tabs = document.querySelector(".active-button")
+        } else {
+            this.tabs = document.querySelector(`.tab-show-hide[data-tab="${this.buttonData}"]`);
+        }
 
+        this.tabs = new Tab(this.tabs);
 
         this.buttonElement.addEventListener("click", this.selectButton.bind(this));
     }
 
     selectButton() { 
-        //let tabData = document.querySelector(".tab-show-hide")
-        //console.log(tabData)
+        // this.tabs = document.querySelectorAll(`.tab-show-hide[data-tab="${this.buttonData}"]`)
 
-        let tabs = document.querySelectorAll(".tab-show-hide")
-                            .forEach((eaTab) => {
-                            eaTab.style.display = "none";
-                            eaTab.classList.remove("default");
-                            tabData.classList.add(".default");
-                            });
+        // let tab = document.querySelectorAll(".tab-show-hide")
+        //                     .forEach((eaTab) => {
+        //                     eaTab.style.display = "none";
+        //                     eaTab.classList.remove("default");
+        //                     this.tabs.classList.add("default")
+        //                     });
 
+        const tabs = document.querySelectorAll(".tab-show-hide")
+                             .forEach((eaTab) => eaTab.style.display = "none");
+
+        this.tabs.selectTab();                    
     }
+}
+
+class Tab {
+    constructor(tabElement) {
+        this.tabElement = tabElement;
+    }
+
+    selectTab() {
+        this.tabElement.style.display = "flex";
+    }
+    
 }
 
 
 let buttons = document.querySelectorAll(".button")
                       .forEach((eaButton) => new ButtonLink(eaButton))
 
+  
 
 
 
