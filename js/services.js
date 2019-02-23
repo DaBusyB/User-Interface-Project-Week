c562b74c7ffd3f1f2ace89cfdd3e1284fa1902f0
Brandi Apetsi <Brandi@Brandis-iMac.local>
2019-02-23 11:34:59 -0500

    working on services page

--- js/services.js
+++ js/services.js
@@ -1,94 +1,53 @@
-class ButtonLink {
-    constructor(buttonElement) {
-        this.buttonElement = buttonElement;
-
-        this.buttonData = this.buttonElement.dataset.button;
-       
-        if(this.buttonData === "defaultButton") {
-            this.tabs = document.querySelector(".active-button")
-        } else {
-            this.tabs = document.querySelector(`.tab-show-hide[data-tab="${this.buttonData}"]`);
-        }
-
-        this.tabs = new Tab(this.tabs);
-
-        this.buttonElement.addEventListener("click", this.selectButton.bind(this));
-    }
-
-    selectButton() { 
-        // this.tabs = document.querySelectorAll(`.tab-show-hide[data-tab="${this.buttonData}"]`)
-
-        // let tab = document.querySelectorAll(".tab-show-hide")
-        //                     .forEach((eaTab) => {
-        //                     eaTab.style.display = "none";
-        //                     eaTab.classList.remove("default");
-        //                     this.tabs.classList.add("default")
-        //                     });
-
-        const tabs = document.querySelectorAll(".tab-show-hide")
-                             .forEach((eaTab) => eaTab.style.display = "none");
-
-        this.tabs.selectTab();                    
-    }
-}
-
-class Tab {
-    constructor(tabElement) {
-        this.tabElement = tabElement;
-    }
-
-    selectTab() {
-        this.tabElement.style.display = "flex";
-    }
-    
-}
-
-
-let buttons = document.querySelectorAll(".button")
-                      .forEach((eaButton) => new ButtonLink(eaButton))
-
-  
-
-
-
 // class ButtonLink {
 //     constructor(buttonElement) {
 //         this.buttonElement = buttonElement;
 
 //         this.buttonData = this.buttonElement.dataset.button;
+       
+//         if(this.buttonData === "defaultButton") {
+//             this.tabs = document.querySelector(".active-button")
+//         } else {
+//             this.tabs = document.querySelector(`.tab-show-hide[data-tab="${this.buttonData}"]`);
+//         }
 
-//         this.buttonData === "defaultButton" ?
-//         this.tabs = document.querySelector(".default") :
-//         this.tabs = document.querySelector(`.tab-show-hide[data-tab="${this.buttonData}"]`);
-
-//         // this.tabs = new Tab();
+//         this.tabs = new Tab(this.tabs);
 
 //         this.buttonElement.addEventListener("click", this.selectButton.bind(this));
 //     }
 
-//     selectButton() {
-//         const button = document.querySelectorAll(".button")
-//                                .forEach((eaButton) => eaButton.classList.remove((".active-button")));
+//     selectButton() { 
+//         // this.tabs = document.querySelectorAll(`.tab-show-hide[data-tab="${this.buttonData}"]`)
 
-//         const tab = document.querySelectorAll(".tab-show-hide")
+//         // let tab = document.querySelectorAll(".tab-show-hide")
+//         //                     .forEach((eaTab) => {
+//         //                     eaTab.style.display = "none";
+//         //                     eaTab.classList.remove("default");
+//         //                     this.tabs.classList.add("default")
+//         //                     });
+
+//         const tabs = document.querySelectorAll(".tab-show-hide")
 //                              .forEach((eaTab) => eaTab.style.display = "none");
-                                 
-//         this.buttonElement.classList.add(".active-button");
 
-        
-       
+//         this.tabs.selectTab();                    
 //     }
 // }
 
-// // class Tab {
-// //     constructor(tabElement) {
-// //         this.tabElement = tabElement;
-// //     }
+// class Tab {
+//     constructor(tabElement) {
+//         this.tabElement = tabElement;
+//     }
 
-// //     selectTab() {
-// //         this.tabElement.style.display = "flex";
-// //     }
-// // }
+//     selectTab() {
+//         this.tabElement.style.display = "flex";
+//     }
+    
+// }
+
 
 // let buttons = document.querySelectorAll(".button")
 //                       .forEach((eaButton) => new ButtonLink(eaButton))
+
+  
+
+
+
